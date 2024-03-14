65b4eb5b049cb08a07f5e07ab33d9c9fe5789ec8
abdul haseeb <mr.arain9199@gmail.com>
2024-03-14 09:51:59 +0500

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+let myName = prompt('enter your name')
+alert ( 'welcome ' + myName)
+let num1 = prompt ('first number')
+let num2= prompt ('second number')
+num1 =Number(num1)
+num2 =Number(num2)
+
+console.log("num 1", num1);
+console.log("num 2", num2);
+let Add = num1 + num2
+let Sub = num1 - num2
+alert('addition:' + Add)
+alert('subtraction:' + Sub)
+alert('multipilication:' + num1 * num2  )
+alert('division:' + num1 / num2  )
